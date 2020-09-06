28500c2ebfebf5a4961996d9532d1c047f90d993
Brian Seaver <bseaver@d155.org>
2020-09-06 16:27:08 -0500

    initial commit

--- NotificationNotes/AppDelegate.swift
+++ NotificationNotes/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+       
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            if let error = error{ print("\(error)")}
+        }
         return true
     }
 
--- NotificationNotes/ViewController.swift
+++ NotificationNotes/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func introNotification()
+    {
+        
+    }
 
-
+    @IBAction func notificationAction(_ sender: UIButton) {
+        // Make sure userNotifications is turned on
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            let status = settings.authorizationStatus
+            if status == .denied || status == .notDetermined{
+                DispatchQueue.main.async {
+                    print("\(status)")
+                }
+            }
+            // 1 Create content
+            let content = UNMutableNotificationContent()
+            content.title = "My Notification"
+            content.body = "You clicked the Add Notification Button"
+            content.userInfo = ["Step": 0]
+            
+            //  2 Create Time Trigger
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+            
+            // 2.1 Create Calendar Trigger
+            var dateComponent = Calendar.current.dateComponents([.hour,.minute,.second], from: Date())
+            dateComponent.second = dateComponent.second! + 5
+            let trigger2 = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
+            
+            // 3 Create Request
+            let request = UNNotificationRequest(identifier: "Time Request", content: content, trigger: trigger)
+            let request2 = UNNotificationRequest(identifier: "Calendar Request", content: content, trigger: trigger2)
+          
+            // 4 Make Request
+            UNUserNotificationCenter.current().add(request2) { (error) in
+                if let error = error{
+                    print("\(error)")
+                }
+                print("Created Request")
+            }
+            
+            
+        }
+    }
+    
 }
 
